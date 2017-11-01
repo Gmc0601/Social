50c2fe7b58c32c1ad6da5621b5679c3f6b2947e8
陈飞 <chenfe.i@163.com>
2017-11-01 13:47:02 +0800

    推送设置跟android一样

--- BaseProject/BaseProject/Classes/My/Controller/XFSettingViewController.m
+++ BaseProject/BaseProject/Classes/My/Controller/XFSettingViewController.m
@@ -30,7 +30,7 @@
 - (void)setupUI {
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.view.backgroundColor = WhiteColor;
-    self.pushArray = @[@"不延迟推送", @"延迟1小时", @"延迟3小时", @"延迟6小时", @"延迟12小时", @"延迟24消失"];
+    self.pushArray = @[@"不延迟推送", @"延迟1小时", @"延迟3小时", @"延迟6小时", @"延迟12小时", @"延迟24小时"];
     UIView *navView = [UIView xf_navView:@"设置"
                               backTarget:self
                               backAction:@selector(backBtnClick)];
@@ -82,7 +82,7 @@
                               NSString *xianshi = infoDict[@"xianshi"];
                               if (self.pushArray.count > xianshi.integerValue) {
                                   self.selectItemView = self.pushArray[xianshi.integerValue];
-                                  [weakSelf setupPushSettingItem];
+                                  [weakSelf setupPushSettingItem:[self.pushArray objectAtIndex:xianshi.integerValue]];
                               }
                           }
                           
@@ -91,13 +91,22 @@
               }];
 }
 
-- (void)setupPushSettingItem {
-    
+- (void)setupPushSettingItem:(NSString *)xianshi {
+    if (xianshi.length) {
+        UILabel *label = [[self.view viewWithTag:0] viewWithTag:100];
+        label.text = xianshi;
+        label.hidden = NO;
+    }
 }
 
 #pragma mark ----------<XFSelectItemViewDelegate>----------
 - (void)selectItemView:(XFSelectItemView *)itemView selectInfo:(NSString *)info {
-    
+    [self setupPushSettingItem:info];
+    NSInteger index = [self.pushArray indexOfObject:info];
+    [HttpRequest postPath:XFResetPushSettingUrl
+                   params:@{@"delay_time" : [NSString stringWithFormat:@"%zd", index]}
+              resultBlock:^(id responseObject, NSError *error) {
+              }];
 }
 
 #pragma mark ----------Action----------
@@ -106,13 +115,14 @@
 }
 
 - (void)viewTap:(UIGestureRecognizer *)ges {
-    FFLog(@"%zd", ges.view.tag);
     if (ges.view.tag == 0) {
         XFSelectItemView *selectItem = [[XFSelectItemView alloc] initWithTitle:@"推送设置"
                                                                      dataArray:self.pushArray
                                                                     selectText:nil];
         selectItem.delegate = self;
         [self.view addSubview:selectItem];
+    } else if (ges.view.tag == 1) {
+        
     } else if (ges.view.tag == 2) {
         XFUserAgreementViewController *controller = [[XFUserAgreementViewController alloc] init];
         controller.agreementType = AgreementType_About;
@@ -137,8 +147,25 @@
                                  numberOfLines:0
                                      alignment:NSTextAlignmentLeft];
     label.text = text;
-    label.frame = CGRectMake(15, 0, view.width - 30, view.height);
+    label.frame = CGRectMake(15, 0, 100, view.height);
     [view addSubview:label];
+    
+    UIImageView *arrowView = [[UIImageView alloc] initWithImage:Image(@"icon_gd-拷贝")];
+    arrowView.centerY = label.centerY;
+    arrowView.right = kScreenWidth - 17;
+    [view addSubview:arrowView];
+    
+    UILabel *rightLabel = [UILabel xf_labelWithFont:Font(12)
+                                          textColor:RGBGray(102)
+                                      numberOfLines:0
+                                          alignment:NSTextAlignmentRight];
+    rightLabel.width = 150;
+    rightLabel.right = arrowView.left - 10;
+    rightLabel.top = 0;
+    rightLabel.height = view.height;
+    rightLabel.tag = 100;
+    rightLabel.hidden = YES;
+    [view addSubview:rightLabel];
     
     UIImageView *imgView = [[UIImageView alloc] initWithImage:Image(@"icon_gd")];
     imgView.centerY = view.height * 0.5;
